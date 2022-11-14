10b60d4b08365a94492bd65ad12c75b50e1652ad
xabier <xabier.sinde@gmail.com>
2022-11-14 21:55:16 +0100

    Added "pandas" module.

--- main.py
+++ main.py
@@ -2,14 +2,19 @@
 #     data = weather_data.readlines()
 #     print(data)
 
-import csv
-with open("weather_data.csv") as weather_data:
-    data = csv.reader(weather_data)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-        print(row)
-    print(temperatures)
+# import csv
+# with open("weather_data.csv") as weather_data:
+#     data = csv.reader(weather_data)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#         print(row)
+#     print(temperatures)
 
+import pandas
 
+data = pandas.read_csv("weather_data.csv")
+print(data)
+temperatures = data["temp"]
+print(temperatures)
