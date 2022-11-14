d2cd94e38430a4ee5d666a20cd2a738632a33191
xabier <xabier.sinde@gmail.com>
2022-11-14 21:45:25 +0100

    Reading CSV files using csv method ie csv.reader()

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# with open("weather_data.csv") as weather_data:
+#     data = weather_data.readlines()
+#     print(data)
+
+import csv
 with open("weather_data.csv") as weather_data:
-    data = weather_data.readlines()
-    print(data)
+    data = csv.reader(weather_data)
+    for row in data:
+        print(row)
